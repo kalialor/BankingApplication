d6d3cf184a49163d80e13be8e33a852624b074be
kalialor <103382223+kalialor@users.noreply.github.com>
2022-07-15 02:51:56 -0400

    Add files via upload

--- FirstBank.java
+++ FirstBank.java
@@ -1,8 +1,15 @@
+package bankingapp;
 
 public class FirstBank {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Account Kalia = new Account("Kalia Lor", "A00001");
+		Kalia.showMenu();
+		
+		//Account Beyonce = new Account("Beyonce", "B00001");
+		//Beyonce.showMenu();
+		
 
 	}
 
